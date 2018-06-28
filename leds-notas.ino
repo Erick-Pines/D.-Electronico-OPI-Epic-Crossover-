d7db7c950acf2e387ee80445b91b06b728a671de
afr2903 <afr102903@gmail.com>
2018-06-27 22:48:33 -0500

    Arreglar rasgueo para notas largas

--- leds-notas.ino
+++ leds-notas.ino
@@ -1,5 +1,6 @@
 int i=0;
 bool boton[] = {0, 0, 0, 0, 0};
+bool anterior[] = {0, 0, 0, 0, 0};
 int p=5;
 bool rasgueo=0;
 int notas[] = {5,6,7,8,11,12};
@@ -11,7 +12,7 @@ void setup() {
 void decode(int coding){
   if(coding<1) return;
   boton[i]= coding%2;
-  if(boton[i++]) rasgueo=1;
+  if(anterior[i]==0 && boton[i++]) rasgueo=1;
   decode(coding/2);
 }
 void loop() {
@@ -20,11 +21,13 @@ void loop() {
     decode(coding);
     for(int j=0; j<5; j++){
       boton[j]==1? digitalWrite(notas[j],HIGH) : digitalWrite(notas[j],LOW);
+      anterior[j] = boton[j];
       boton[j]=0;
     }
-    if(rasgueo) digitalWrite(notas[5],HIGH);
-    else digitalWrite(notas[5],LOW);
+    if(rasgueo) digitalWrite(notas[5],HIGH); 
     i=0, rasgueo=0;
-    //delay(100);
+    delay(10);
+    digitalWrite(notas[5],LOW);
   }
+  
 }
